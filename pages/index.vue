c366c5854b5ea557c8163480e9ab6b93d264b9ca
selcuk tatar <selcuk.tatar@outlook.com>
2025-02-13 11:45:45 +0700

    Demo Instruction component created

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,5 @@
 <template>
-  <v-container style="">
-    <v-row justify="center">
-      <v-col cols="5">
-        <!-- <v-img src="/Logo-Wide.png"
-               class="pb-12"
-               width="100%"
-               contain></v-img> -->
-        <v-card>
-          <v-card-title class="headline">Welcome To The GAME1328 Space Wars
-            Demo
-          </v-card-title>
-          <v-card-text>
-            <h2>About The Game</h2>
-            <p classs="body-2">
-              Space Wars is a collection of sapce themed action adventure &
-              puzzle minigames where the main objective is typically to avoid
-              alien abduction.
-            </p>
-            <h2>How To Play</h2>
-            <p classs="body-2">
-              Everybody wants the best tech but few are willing to put in the
-              work for it.
-              The Asteroid Mine is by far the best place to mine the ore you
-              need to upgrade or
-              purchase new equipment.
-              <br /> <br />
-              Click on cells in the Asteroid Mine to check for ore. You can
-              process the ore into
-              new items and equipment from your Inventory View.
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <DemoInstructions />
 </template>
 
 <script>
