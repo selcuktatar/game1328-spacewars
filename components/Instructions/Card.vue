f34f02bfd1269653e6f9b1034529ac7fa60dac5b
selcuk tatar <selcuk.tatar@outlook.com>
2025-02-13 12:20:17 +0700

    state created for messages, texts

--- components/Instructions/Card.vue
+++ components/Instructions/Card.vue
@@ -1,28 +1,15 @@
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data() {
-    return {
-      mainHeadline: 'Welcome To The GAME1328',
-      subtitle: 'Space Wars Demo',
-      gameRules: [
-        {
-          id: 1,
-          header: 'About The Game',
-          rule: 'Space Wars is a collection of sapce themed action adventure & puzzle minigames where the main objective is typically to avoid alien abduction.',
-        },
-        {
-          id: 2,
-          header: 'How To Play',
-          rule: 'Everybody wants the best tech but few are willing to put in the work for it. The Asteroid Mine is by far the best place to mine the ore you need to upgrade or purchase new equipment.',
-        },
-        {
-          id: 3,
-          header: '',
-          rule: 'Click on cells in the Asteroid Mine to check for ore. You can process the ore into new items and equipment from your Inventory View.',
-        },
-      ]
-    }
-  }
+    return {}
+  },
+  computed: {
+    ...mapGetters({ mainHeadline: 'messages/InstructionMainHeadline' }),
+    ...mapGetters({ subtitle: 'messages/InstructionSubtitle' }),
+    ...mapGetters({ gameRules: 'messages/InstructionGameRules' }),
+  },
 }
 </script>
 <template>
