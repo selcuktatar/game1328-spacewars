0b70b02257b625d8ac2645280ac3082447ce1289
selcuk tatar <selcuk.tatar@outlook.com>
2025-02-13 13:09:02 +0700

    title usage in rankPoint component

--- pages/Fleet-Inventory/index.vue
+++ pages/Fleet-Inventory/index.vue
@@ -3,15 +3,13 @@
     <v-row justify="center" style="min-height: 100vh">
       <v-col cols="12" lg="3">
         <v-card style="width: 100%">
-          <InventoryRankPoints />
+          <InventoryRankPoints title="Rank Point" />
           <InventoryPlayerItems />
         </v-card>
       </v-col>
 
       <v-col cols="12" lg="7">
-        <v-card style="width: 100%">
           <InventoryMarketItems />
-        </v-card>
       </v-col>
     </v-row>
   </section>
