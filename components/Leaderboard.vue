015cf90300fa25cce3ea90a7d05ced364e1e086c
selcuk tatar <selcuk.tatar@outlook.com>
2025-02-14 02:15:55 +0700

    leaderboard data fetching added into component

--- components/Leaderboard.vue
+++ components/Leaderboard.vue
@@ -1,18 +1,28 @@
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-    data() {
-        return {}
-    },
-    computed: {
-      ...mapGetters({ allItems: 'game/AllItems' }),
-    },
-    methods: {
-      getFirstLetters(name) {
-        return name[0]
-      }
+  async fetch() {
+    await this.getLeaderboardData()
+  },
+  data() {
+    return {
+      leaderboardData: []
     }
+  },
+  computed: {
+    sortedLeaderboardData() {
+      return this.leaderboardData.slice().sort((a, b) => b.point - a.point)
+    }
+  },
+  methods: {
+    getFirstLetters(name) {
+      return name[0]
+    },
+    async getLeaderboardData() {
+      const documents = await this.$fire.firestore.collection('leaderboard').get()
+      this.leaderboardData = documents.docs.map(doc => doc.data())
+    }
+  }
 }
 </script>
 <template>
@@ -20,8 +30,8 @@ export default {
     <v-card>
       <v-card-title class="font-weight-bold">Leaderboard</v-card-title>
       <v-divider></v-divider>
-      <v-list>
-        <v-list-item v-for="(item, index) in allItems" :key="`item-${index}`">
+      <v-list v-if="sortedLeaderboardData.length > 0">
+        <v-list-item v-for="(item, index) in sortedLeaderboardData" :key="item.id">
           <v-list-item-avatar>
             <v-avatar size="36" color="info">
               <span class="white--text text-h5">{{ getFirstLetters(item.name) }}</span>
@@ -31,10 +41,14 @@ export default {
             {{ item.name }}
           </v-list-item-title>
           <v-layout class="justify-end">
-            {{ item.cost }}
+            {{ item.point }}
           </v-layout>
         </v-list-item>
       </v-list>
+      <div v-else>
+        No leaderboard data found.
+      </div>
     </v-card>
   </v-container>
 </template>
+
